95382a83569830681187b560e122d23f08ce6086
turtleDev <saravjeetamansingh@gmail.com>
2015-08-09 17:18:28 +0530

    Add copyright info.

--- src/debug.h
+++ src/debug.h
@@ -1,5 +1,26 @@
 /*
- *      debug.h -- debug macros.
+ *      Copyright (C) 2015 Saravjeet 'Aman' Singh
+ */
+
+/*   This file is a part of Comrade.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+
+/*
+ *      debug.h - debug macros.
  *
  *  This is an adaptation of Zed A. Shaw's debug macro header, from his
  *  book "learn C the hard way".
