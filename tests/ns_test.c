e3cf8380cf6ae68ee48d24a7df7577070ee6a938
turtleDev <saravjeetamansingh@gmail.com>
2016-01-08 10:29:01 +0530

    update tests

--- tests/ns_test.c
+++ tests/ns_test.c
@@ -7,17 +7,17 @@ int main() {
     char *f = "test.wav";
     
     printf("Playing sound\n");
-    play_notification_sound(f);
+    notification_sound_play(f);
 
     printf("Playing Music\n");
     char *f2 = "/home/pitchblack/Programming/Projects/Comrade/tests/song.mp3";
-    play_notification_sound(f2);
+    notification_sound_play(f2);
 
     printf("Checking error checking:\n");
     printf("trying to pass wrong arg: test.wav2\n");
 
     char *f3 = "test.wav2";
-    if(play_notification_sound(f3) == -1) {
+    if(notification_sound_play(f3) == -1) {
         log_err("Error opening %s", f3);
     }
 
